c9306c9659655666172501512d8ae8d5b9ff8435
Ryan Edwards <ryanmedwards@hotmail.com>
2014-05-09 15:11:56 -0400

    Added form for new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,16 @@
 class CommentsController < ApplicationController
   before_filter :load_commentable
 
-  def index
-    @comments = @commentable.comments.all
-  end
+  # def index
+  #   @comments = @commentable.comments.all
+  # end
 
   def new
     @comment = @commentable.comments.new
   end
 
   def create
-    @comment = @commentable.comments.new
+    @comment = @commentable.comments.build(comment_params)
     respond_to do |format|
       if @comment.save
         format.html {redirect_to polymorphic_path(@commentable), notice: "Message added." }
@@ -19,6 +19,7 @@ class CommentsController < ApplicationController
           format.html {render polymorphic_path}
           format.js {}
       end
+    end
   end
 
   def destroy
@@ -28,8 +29,9 @@ class CommentsController < ApplicationController
   end
 
   private
-  def load_commentable
-    resource, id = request.path.split('/')[1, 2]
-    @commentable = resource.singularize.classify.find(id)
+  def comment_params
+    params.require(:comment).permit(:message, :commentable_id, :commentable_type)
   end
+
+  
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,6 +14,7 @@ class ProjectsController < ApplicationController
 		if current_user
 			@pledge = @project.pledges.build
 			@comments = @commentable.comments
+			@comment = @commentable.comments.build
 		end
 	end
 
