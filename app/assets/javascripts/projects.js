3f554b2ca76e7a5972d6d2f754eaf63a3d2adc6c
Ryan Edwards <ryanmedwards@hotmail.com>
2014-05-09 16:27:59 -0400

    Added Ajax Handling functions to messages

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -26,8 +26,20 @@ $(document).ready(function(){
 			dataType: 'script',
 			data: data
 		});
-
 	});
 
-
+	$(function() {
+     $("#new_comment")
+        .on('ajax:beforeSend', function() { // set this code to happen just before the ajax request is made
+          $("input[type='submit']") // make changes to the submit button
+            .val('Saving...') // change the text on the button
+            .attr('disabled', 'disabled'); // disable the button
+        })
+        .on('ajax:complete', function() {
+          $("input[type='submit']")
+            .val('Create Review')
+            .removeAttr('disabled');
+        });
+  });
+  
 });
